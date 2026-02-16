c018f59d15e8eb806a71d30e05fef1b777f56641
Stevenson <stevensonstlaurent08@gmail.com>
2026-02-15 20:16:31 -0800

    nasted layout

--- app/account/page.js
+++ app/account/page.js
@@ -1,9 +1,14 @@
 export const metadata = {
-  title: "Account",
+  title: "Dancer area",
 };
 
 const Page = () => {
-  return <h2>account page</h2>;
+  return (
+    <h2 className="font-semibold text-2xl text-accent-400 mb-7">
+      Welcome to your dancer area! Choose an option from the menu to get
+      started.
+    </h2>
+  );
 };
 
 export default Page;
