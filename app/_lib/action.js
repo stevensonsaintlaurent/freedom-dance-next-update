782f6d3f82cea4c67a5e0b0ed55b9ddde00737b6
Stevenson <stevensonstlaurent08@gmail.com>
2026-03-04 14:45:31 -0800

    delete reservation

--- app/_lib/action.js
+++ app/_lib/action.js
@@ -1,6 +1,8 @@
 "use server";
+import { revalidatePath } from "next/cache";
 import { auth, signIn, signOut } from "./auth";
 import { supabase } from "./supabase";
+import { getBookings } from "./data-service";
 
 // ======================== Update Profile =========================
 export async function updateProfile(formData) {
@@ -21,7 +23,24 @@ export async function updateProfile(formData) {
 export async function signInAction() {
   await signIn("google", { redirectTo: "/account" });
 }
+// ======================== Delete Reservation =========================
+export async function deleteReservation(bookingId) {
+  const session = await auth();
+  if (!session) throw new Error("You must log in ");
+  const { data, error } = await supabase.from("bookings").delete().eq("id", id);
+  const guestBooking = await getBookings(session.user.guestId);
+  const guestBookingsIds = guestBookingsIds.map((booking) => booking.id);
 
+  if (!guestBookingsIds.includes(bookingId))
+    throw new Error("You are not allowed to delete this booking");
+  if (error) {
+    console.error(error);
+    throw new Error("Booking could not be deleted");
+  }
+  revalidatePath("/account/reservations");
+}
+
+// ======================== Sign Out =========================
 export async function signOutAction() {
   await signOut({ redirectTo: "/" });
 }
